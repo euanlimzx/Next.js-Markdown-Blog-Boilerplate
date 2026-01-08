2bd61605215f9487b71d2c7c0f350e2d1c1831a2
euanlimzx <euanlimzx@gmail.com>
2026-01-08 15:24:50 -0800

    feat: footer changes

--- app/config/links.ts
+++ app/config/links.ts
@@ -1,6 +1,6 @@
 export const footerLinks = {
-  "https://www.linkedin.com/": { name: "LinkedIn" },
-  "https://x.com/home": { name: "X" },
+  "https://www.linkedin.com/in/euanlimzx/": { name: "LinkedIn" },
+  "https://x.com/euanlimzx": { name: "X" },
 };
 
 export const navLinks = {
